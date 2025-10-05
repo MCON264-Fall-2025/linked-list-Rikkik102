5aa0f2197d596a04c4404c8b881e41b7a5f02b33
Rivka Kaufman <rkaufman8@student.touro.edu>
2025-10-05 11:25:14 -0400

    implemeted detectCycleInfo method
    added the CycleInfo class and wrote the method to detect cycle info

--- StackADT/src/main/java/linkedlist/LinkedListCycleAnalyzer.java
+++ StackADT/src/main/java/linkedlist/LinkedListCycleAnalyzer.java
@@ -5,7 +5,37 @@ import support.CycleInfo;
 import support.LLNode;
 
 public class LinkedListCycleAnalyzer<T> {
+
+
     public static <T> CycleInfo detectCycleInfo(LLNode<T> head){
-        throw new NotImplementedException("TODO: implement this method");
+       int index = 0;
+       LLNode<T> pointer1 = head;
+       LLNode<T> pointer2 = head;
+       pointer1 = pointer1.getLink();
+        if (pointer1 == null) {
+            return new CycleInfo(-1, 0);
+        }
+       while (!(pointer1.equals(pointer2))) {
+           LLNode<T> jump = pointer2.getLink();
+           pointer2= jump.getLink();
+           if (pointer1.equals(pointer2)) {
+               break;
+           }
+           pointer1 = pointer1.getLink();
+           if (pointer1 == null) {
+               return new CycleInfo(-1, 0);
+           }
+           index++;
+       }
+       if (pointer1.equals(head)) {
+           index = 0;
+       }
+        int cycleLength = 1;
+            pointer1 = pointer1.getLink();
+            while (!(pointer1.equals(pointer2))) {
+                pointer1 = pointer1.getLink();
+                cycleLength++;
+            }
+        return new CycleInfo(index, cycleLength);
     }
 }
